b543f4585cbf6c3a7060cdfc0dde9aeeb2571bc9
roppy001 <roppy.dreamvision200@gmail.com>
2024-07-14 01:57:07 +0900

    update seal p4 info

--- docs/js/dtcore.js
+++ docs/js/dtcore.js
@@ -107,7 +107,7 @@ var SEAL_DATA = [
   { title: '調査中5 テラシ、ホラー', index: 23, page: 3, rarity: 2 },
   { title: '調査中6 トラ', index: 27, page: 3, rarity: 0 },
   { title: '調査中7 タコ、ヤドカシ', index: 29, page: 3, rarity: 1 },
-  { title: '調査中8 つっぱり、ちゃがま', index: 32, page: 3, rarity: 1 },
+  { title: '調査中8 もも、つっぱり、ちゃがま', index: 32, page: 3, rarity: 1 },
   { title: '調査中9 トラ、テラシ', index: 34, page: 3, rarity: 0 },
   { title: '調査中10 タコ、ホラー', index: 39, page: 3, rarity: 2 },
   { title: '調査中11 もも、ちゃがま、ホラー', index: 42, page: 3, rarity: 2 },
